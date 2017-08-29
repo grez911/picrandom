02b1f9865da1496304e101b6f371a1ef48a60cac
grez911 <grez911@gmail.com>
2017-08-29 11:22:16 +0300

    Updated tests data

--- utils/blns.py
+++ utils/blns.py
@@ -4,43 +4,60 @@ import os
 import shutil
 import sys
 
+def unicode_truncate(s, length=165):
+    while (sys.getsizeof(s) > length):
+        s = s[:-1]
+    return s
+
 try:
-    shutil.rmtree("data")
+    shutil.rmtree("complex_names")
 except:
     pass
-os.mkdir("data")
+os.mkdir("complex_names")
 
 with open("blns.json", "r") as f:
     j = f.read()
 
 names = json.loads(j)
-os.chdir("data")
+os.chdir("complex_names")
 
 dirs = set()
 files = set()
+failed = set()
 
 i = 1
 
 for name in names:
-    if '/' in name:
+    _name = name
+    if '/' in _name or _name == '.' or _name == '':
         continue
     try:
-        os.mkdir(name)
-        dirs.add(name)
-    except:
-        pass
+        os.mkdir(_name)
+        dirs.add(_name)
+    except Exception as err1:
+        if "File name too long" in str(err1):
+            try:
+                os.mkdir(unicode_truncate(name))
+                _name = unicode_truncate(name)
+                dirs.add(_name)
+            except Exception as err2:
+                failed.add(_name)
+                print(err2)
+                continue
     try:
-        with open("%s/%s" % (name, name), "w") as f:
+        with open("%s/%s" % (_name, _name), "w") as f:
             f.write(str(i))
-            files.add(name)
-        with open("%s/test.txt" % name, "w") as f:
+        with open("%s/test.txt" % _name, "w") as f:
             f.write(str(i))
+        files.add(_name)
         i += 1
-    except:
+    except Exception as err3:
+        print(err3)
         pass
 
 with open("test.txt", "w") as f:
     f.write("root")
 
+print(failed)
 print(dirs.difference(files))
 print(files.difference(dirs))
